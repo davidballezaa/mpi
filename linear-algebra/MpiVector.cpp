e791f1a730815f95a9626e4f8d3f30673dfda69c
David Balleza Ayala <davidballezaa@gmail.com>
2023-08-21 14:44:09 -0600

    add constructor and destructor to vector class

---
+++ linear-algebra/MpiVector.cpp
@@ -0,0 +1,35 @@
+#include <mpi.h>
+#include <cmath>
+#include <cassert>
+
+class MpiVector
+{
+    private:
+        int mLo, mHi, mSize;
+        double* mData;
+        double* mGlobalData;
+    public:
+        MpiVector(int vecSize){
+            int num_procs = MPI::COMM_WORLD.Get_size();
+            int rank = MPI::COMM_WORLD.Get_rank();
+            int ideal_local_size = vecSize / num_procs;
+
+            assert(ideal_local_size > 0);
+            mLo = ideal_local_size*rank;
+            mHi = ideal_local_size*(rank + 1);
+
+            if(rank == num_procs - 1){
+                mHi = vecSize;
+            }
+
+            assert(mHi > mLo);
+            mData = new double[mHi - mLo];
+            mGlobalData = new double[vecSize];
+            mSize = vecSize;
+        }
+
+        ~MpiVector() {
+            delete[] mData;
+            delete[] mGlobalData;
+        }
+};
